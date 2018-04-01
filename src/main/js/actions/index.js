aeb09bf6cea21b8cb42d92e55b350f3aaf43aeaf
Evgeny Kholukhoev <eekholukhoev@edu.hse.ru>
2018-04-01 20:09:36 +0300

    HSECWS-42 Implemented: 1. Synchronous actions 2. Reducers for actions

--- src/main/js/actions/index.js
+++ src/main/js/actions/index.js
@@ -1,23 +1,47 @@
-let nextTodoId = 0;
+// Тип действия, отвечающий за выбор группы студентов пользователем.
+export const SELECT_GROUP = 'SELECT GROUP';
+// Тип действия, который отвечает за обновление данных в группе.
+export const INVALIDATE_GROUP = 'INVALIDATE_GROUP';
 
-export const addTodo = text => ({
-    type: 'ADD_TODO',
-    id: nextTodoId++,
-    text
-});
 
-export const setVisibilityFilter = filter => ({
-    type: 'SET_VISIBILITY_FILTER',
-    filter
-});
+// генератор действия выбора группы.
+export function selectGroup(group) {
+    return {
+        type: SELECT_GROUP,
+        group
+    }
+}
 
-export const toggleTodo = id => ({
-    type: 'TOGGLE_TODO',
-    id
-});
+export function invalidateGroup(group) {
+    return {
+        type: INVALIDATE_GROUP,
+        group
+    }
+}
 
-export const VisibilityFilters = {
-    SHOW_ALL: 'SHOW_ALL',
-    SHOW_COMPLETED: 'SHOW_COMPLETED',
-    SHOW_ACTIVE: 'SHOW_ACTIVE'
-};
+
+// Тип действия, отвечающий за фетч данных
+export const STUDENTS_SUM_WORKLOAD_REQUEST = 'STUDENTS_SUM_WORKLOAD_REQUEST';
+export const STUDENTS_SUM_WORKLOAD_RECEIVE = 'STUDENTS_SUM_WORKLOAD_RECEIVE';
+
+
+// Когда надо будет фетчить данные о загрузке загруженностей студентов для группы,
+// посылаем действие STUDENTS_SUM_WORKLOAD_REQUEST.
+// TODO: 1. Добавить fromDate, toDate.
+export function requestStudentsSumWorkload(group) {
+    return {
+        type: STUDENTS_SUM_WORKLOAD_REQUEST,
+        group
+    }
+}
+
+// Когда сетевой запрос будет осуществлен, мы отправим действие STUDENTS_SUM_WORKLOAD_RECEIVE
+// TODO: 1. Добавить fromDate, toDate.
+export function studentsSumWorkloadReceive(group, json) {
+    return {
+        type: STUDENTS_SUM_WORKLOAD_RECEIVE,
+        group,
+        students: json.data,
+        receivedAt: Date.now()
+    }
+}
--- src/main/js/reducers/index.js
+++ src/main/js/reducers/index.js
@@ -1,8 +1,62 @@
-import { combineReducers } from 'redux'
-import todos from './todos'
-import visibilityFilter from './visibilityFilter'
+import {combineReducers} from "redux";
 
-export default combineReducers({
-    todos,
-    visibilityFilter
-})
+import {
+    SELECT_GROUP,
+    INVALIDATE_GROUP,
+    STUDENTS_SUM_WORKLOAD_REQUEST,
+    STUDENTS_SUM_WORKLOAD_RECEIVE
+} from '../actions'
+
+function selectedGroup(state = {}, action) {
+    switch (action.type) {
+        case SELECT_GROUP:
+            return action.group;
+        default:
+            return state;
+    }
+}
+
+function students(state = {
+    isFetching: false,
+    didInvalidate: false,
+    items: []
+}, action) {
+    switch (action.type) {
+        case INVALIDATE_GROUP:
+            return Object.assign({}, state, {didInvalidate: true});
+        case STUDENTS_SUM_WORKLOAD_REQUEST:
+            return Object.assign({}, state, {
+                isFetching: true,
+                didInvalidate: false,
+            });
+        case STUDENTS_SUM_WORKLOAD_RECEIVE:
+            return Object.assign({}, state, {
+                isFetching: false,
+                didInvalidate: false,
+                items: action.students,
+                lastUpdated: action.receivedAt
+            });
+        default:
+            return state;
+    }
+}
+
+function studentsByGroup(state, action) {
+    switch (action.type) {
+        case INVALIDATE_GROUP:
+        case STUDENTS_SUM_WORKLOAD_REQUEST:
+        case STUDENTS_SUM_WORKLOAD_RECEIVE:
+            return Object.assign({}, state, {
+                [action.group]: students(state[action.group], action)
+            });
+        default:
+            return state;
+    }
+}
+
+const rootReducer = combineReducers({
+    studentsByGroup,
+    selectedGroup
+});
+
+export default rootReducer;
