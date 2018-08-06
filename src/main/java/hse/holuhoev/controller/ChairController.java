f839cc5f9c46aaa0a6ac81ea9d028db8e44ecc2c
Evgeny Kholukhoev <eekholukhoev@edu.hse.ru>
2018-08-06 23:56:05 +0300

    1. CORS

--- src/main/java/hse/holuhoev/controller/ChairController.java
+++ src/main/java/hse/holuhoev/controller/ChairController.java
@@ -3,10 +3,12 @@ package hse.holuhoev.controller;
 import hse.holuhoev.datasource.ChairDatasource;
 import hse.holuhoev.datasource.util.DataSourceResult;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 @RequestMapping("**/api/chair")
 public class ChairController {
     private final ChairDatasource chairDatasource;
