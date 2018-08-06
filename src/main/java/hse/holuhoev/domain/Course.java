80a1dd6cabdef4d5d15dcc7c88256657a1c2fc93
Evgeny Kholukhoev <eekholukhoev@edu.hse.ru>
2018-08-06 19:53:22 +0300

    1. Fix small bugs

--- src/main/java/hse/holuhoev/domain/Course.java
+++ src/main/java/hse/holuhoev/domain/Course.java
@@ -1,10 +1,10 @@
 package hse.holuhoev.domain;
 
 public enum Course {
-    FIRST, SECOND, THIRD, FOURTH, FIFTH;
+    FIRST, SECOND, THIRD, FOURTH, FIFTH, SIX;
 
     public static Course of(int course) {
-        if (course < 1 || course > 5) {
+        if (course < 1 || course > 6) {
             throw new IllegalArgumentException("Invalid value for Course:" + course);
         }
         return Course.class.getEnumConstants()[course - 1];
--- src/main/java/hse/holuhoev/ruz/URL.java
+++ src/main/java/hse/holuhoev/ruz/URL.java
@@ -4,7 +4,7 @@ package hse.holuhoev.ruz;
  * @author Evgeny Kholukhoev
  */
 public class URL {
-    public final static String URL = "http://ruz.hse.ru/RUZService.svc/";
+    public final static String URL = "http://92.242.58.221/ruzservice.svc/";
     public final static Integer LECTURER_LESSON_TYPE = 1;
     public final static int MAX_QUERY_DAYS = 180;
 }
